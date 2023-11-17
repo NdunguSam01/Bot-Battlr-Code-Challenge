302f8f419ad08679db9532cb8345bfc88686fb87
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-17 15:37:31 +0300

    Added some styling to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import './css/Bots.css'
 import Army from './components/Army';
 import BotCollection from './components/BotCollection';
 
